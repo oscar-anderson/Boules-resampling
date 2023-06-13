5c4ae4335283fa5dd38ae83ca97a6a6e0b8f1859
Oscar Anderson <117745490+oscar-anderson@users.noreply.github.com>
2023-06-14 00:50:29 +0100

    Update boulesResampling.m

--- Code/boulesResampling.m
+++ Code/boulesResampling.m
@@ -1,17 +1,18 @@
 %% Simulate the games of Boules and run the resampling test.
 function [pValue, testStats, dist] = boulesResampling()
 
-% Description:
-%     boulesResampling runs all of the functions to simulate the games of
-%     Boules and then test for a statistically significant difference
-%     between the scores of the 'poorly-skilled' (bad) player and
-%     'moderately-skilled' (standard) player.
+% This function uses the functions defined above to simulate five games of
+% Boules between the two players, throwing 10 balls per game, then run the
+% resampling test to examine whether the bad player indeed performs
+% significantly worse than the standard player. The sampling distribution
+% is visualised as a histogram and is output alongside the test statistic
+% and significance of the observed bad vs. standard player score
+% difference.
 
 % Outputs:
-%     pValue = the significance of the observed performance difference.
-%     testStats = the level of difference between scores observed and
-%     those expected under the null hypothesis.
-%     dist = histogram presenting the sampling distribution.
+%     pValue = the significance of the observed score difference.
+%     testStats = the observed bad vs. standard score difference.
+%     dist = a histogram depicting the standard vs. standard sampling distribution.
 
 % Specify number of games to be played between bad and standard player.
 nGame = 5;
@@ -28,7 +29,7 @@ LawnSize.yLowerLeft = 5;
 % Shuffle seed to be used by TruncatedGaussian function's random number generator.
 rng('shuffle');
 
-% Call testBadPlayer function to run the games.
+% Call testBadPlayer function to run the games and resampling test.
 [pValue, testStats, dist] = testBadPlayer(nGame, nBalls, LawnSize);
 
 % Visualise distribution as histogram.
