72255044aef1070155cafef37a042da8bb526e95
Oscar Anderson <117745490+oscar-anderson@users.noreply.github.com>
2023-06-14 01:14:53 +0100

    Update testBadPlayer.m

--- Code/testBadPlayer.m
+++ Code/testBadPlayer.m
@@ -1,9 +1,12 @@
 %% Perform resampling test of difference between player scores.
-
 function [pValue, testStats, dist] = testBadPlayer(nGame, nBalls, LawnSize)
 
-% [pValue, testStats, dist] = testBadPlayer(nGame, nBalls, LawnSize)
-% Simulates specified number of games between bad and standard player.
+% This function runs a game of Boules between a bad and a standard player,
+% generates a sampling distribution of score differences from 1000 sessions
+% of games between two standard players, then determines whether the
+% observed score difference is significantly different to those established
+% from standard vs. standard games, to establish whether the bad player's
+% ability is indeed significantly worse than a standard player's.
 
 % Inputs:
 %    nGame = number of games to be played.
@@ -21,11 +24,15 @@ badVsStandardScore = playBadvStandard(nGame, nBalls, LawnSize);
 % Calculate observed score difference between players from games played.
 testStats = badVsStandardScore(1) - badVsStandardScore(2);
 
-% Generate sampling distribution from 1000 sessions of simulated games.
+% Generate sampling distribution of games between standard players.
 dist = genSamplingDist(nGame, nBalls, LawnSize);
 
-% Calculate proportion of across-game score differences greater than
-% observed score difference, to give significance of observed difference.
+% Compare observed bad vs standard score difference with sampling
+% distribution to determine significance of observed difference.
+
+% Calculate proportion of score differences in standard vs. standard 
+% sampling distribution greater than observed difference, to determine
+% significance of observed standard vs. bad score difference.
 pValue = (sum(dist > testStats)/1000);
 
 end
